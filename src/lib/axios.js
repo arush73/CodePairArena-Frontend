6a451f0f520805fbecf8e41715154bfcb43cc98f
Arush Choudhary <arushgzbin@gmail.com>
2025-10-25 00:23:18 +0530

    commit after seperating the mail service

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  // baseURL: "https://leetclone-next-backend-production.up.railway.app/api/v1",
+  baseURL: "https://leetclone-next-backend-production.up.railway.app/api/v1",
   // baseURL: "http://localhost:8080/api/v1",
-  baseURL: "https://15.207.112.103/api/v1",
+  // baseURL: "https://15.207.112.103/api/v1",
   withCredentials: true,
 });
